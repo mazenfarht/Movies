ffdbfe16e64ce011fd882c3a9c89d8d61ae925f8
mazenfarht <mazenmostafa2022@gmail.com>
2026-05-26 00:52:32 +0300

    fix GlobalApi structure

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -3,11 +3,10 @@ import axios from "axios";
 const movieBaseUrl = "https://api.themoviedb.org/3";
 const api_key = "2108e007807af56b3588464934bf6a69";
 
-// function ترجع request
-const getTrendingVideos = () =>
-  axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
+const getTrendingVideos = axios.get(
+  `${movieBaseUrl}/trending/all/day?api_key=${api_key}`
+);
 
-// export object بشكل صحيح
 const GlobalApi = {
   getTrendingVideos,
 };
